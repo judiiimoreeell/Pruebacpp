b54f39df0eee3edc6b533e4787fa054f569f4964
Judith Morell <judit.morell@enti.cat>
2023-11-24 10:53:56 +0100

    Primer archivo en CPP

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char** argv){
+	//std::cout << argc << std::endl;
+	if(argc > 1){
+	std::cout<< "Holi"  << argv[1] <<"!"<< std::endl;
+	}
+	else{
+	std::cout	<< "Your dck is now a noodle!" << std::endl;
+	}
+	return 0;
+}
